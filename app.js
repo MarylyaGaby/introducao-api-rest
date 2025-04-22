4e5638f61ea4990a02497c986d9e31c2e1f4df3b
Marilia Gabriele <mariliagabriele081@gmail.com>
2025-04-22 16:17:28 -0300

    refact|feat: multi routes de usuários e fornecedores

--- app.js
+++ app.js
@@ -1,49 +1,30 @@
 import express from 'express'
+
+import usuariosRoutes from './routes/usuarios.js'
+
+import fornecedoresRoutes from "./routes/fornecedores.js"
+
 const app = express()
 
 //Permitir ler JSON no corpo da requisição
 app.use(express.json())
+// Adiciona o roteador de usuários
+app.use("/usuarios", usuariosRoutes)
+
+// Adiciona o roteador de fornecedores
+app.use("/fornecedores", fornecedoresRoutes)
 
 const port = 3000
-//Banco de dados Fake (em memória)
-const usuarios = [
-  {id: 1, nome: 'João', email:"joao@email.com"},
-  {id: 2, nome: 'Ana', email:"ana@email.com"},
-]
 
 
 app.get("/", (req, res) => {  
   res.send('Bem vindo a minha API!')
 })
-/*
+// Rota inicio
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-*/
-/*app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-/*s
-/**
- * Crie uma rota(endpoint) do tipo get que
- * envie uma resposta (mensagem) com todos os
- * usuários cadastrados no "banco de dados fake"
- */
-
-app.get('/usuarios', (req, res) => {
-  res.send(usuarios);
-});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log("Example app listening on port ${port}")
 })
-
-/*app.post("/criarUsuario", (req, res)=> {
-  const {nome, sobrenome} = req.body
-  res.send(`Nome: ${nome} | Sobrenome: ${sobrenome}`)
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-*/
